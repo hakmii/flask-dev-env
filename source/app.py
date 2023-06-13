a1e13f91a6f96723b73a14208840bbac24605cf8
hakmii <reg+github@hakkinen.me>
2023-06-13 18:47:03 +0000

    test ci-cd pipeline

--- source/app.py
+++ source/app.py
@@ -10,5 +10,9 @@ def hello():
 def info():
     return 'Here comes some info!'
 
+@app.route('/autodeploy')
+def autodeploy():
+    return 'It just works?'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
